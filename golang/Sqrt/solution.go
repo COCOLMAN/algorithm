1c6e3e70c5c614fc1d1ed9250437dddf8468bda5
soul <soul.h@kakaocorp.com>
2022-12-10 22:42:48 +0900

    done

---
+++ golang/Sqrt/solution.go
@@ -0,0 +1,26 @@
+package Sqrt
+
+import (
+	"math"
+)
+
+func mySqrt(x int) int {
+	maxN := x
+	minN := 0
+	var n int
+	for true {
+		n = (maxN + minN) / 2
+		a := int(math.Pow(float64(n), 2))
+		b := int(math.Pow(float64(n+1), 2))
+		if a <= (x) && b > (x) {
+			break
+		}
+		if a > (x) {
+			maxN = n
+		} else {
+
+			minN = n
+		}
+	}
+	return n
+}
