a1ebd15b2e48f29f9e73f1b850136c4f66996110
COCOLMAN <iam@hyunsol.com>
2023-01-20 23:11:25 +0900

    too slow

--- golang/codility/MaxCounters/solution.go
+++ golang/codility/MaxCounters/solution.go
@@ -1,56 +1,29 @@
 package MaxCounters
 
-func getMaxNumber(A []int, m int) int {
-	hash := map[int]int{}
-	base := 0
+func max(A []int) int {
+	maxN := A[0]
+	for _, n := range A {
+		if n > maxN {
+			maxN = n
+		}
+	}
+	return maxN
+}
 
-	for i := len(A) - 1; i >= 0; i-- {
-		n := A[i]
-		if n == m {
-			base += getMaxNumber(A[:i], m)
-			break
-		}
-		if _, ok := hash[n]; !ok {
-			hash[n] = 0
-		}
-		hash[n]++
+func solution(N int, A []int, base int) []int {
+	list := make([]int, N)
+	for i := 0; i < N; i++ {
+		list[i] = base
 	}
-	maxN := 0
-	for key := range hash {
-		val := hash[key]
-		if maxN < val {
-			maxN = val
+	for idx, a := range A {
+		if a == N+1 {
+			return solution(N, A[idx+1:], max(list))
 		}
+		list[a-1]++
 	}
-	return base + maxN
+	return list
 }
 
 func Solution(N int, A []int) []int {
-	var lastMaxCounterIdx int = 0
-	var existMaxCounter = false
-
-	for i := len(A) - 1; i >= 0; i-- {
-		if A[i] == N+1 {
-			lastMaxCounterIdx = i
-			existMaxCounter = true
-			break
-		}
-	}
-
-	base := getMaxNumber(A[:lastMaxCounterIdx], N+1)
-	answer := make([]int, N)
-	for i := 0; i < N; i++ {
-		answer[i] = base
-	}
-
-	if lastMaxCounterIdx == len(A)-1 {
-		if existMaxCounter {
-			return answer
-		}
-		lastMaxCounterIdx -= 1
-	}
-	for _, inc := range A[lastMaxCounterIdx+1:] {
-		answer[inc-1]++
-	}
-	return answer
+	return solution(N, A, 0)
 }
