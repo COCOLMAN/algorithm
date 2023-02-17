9538c294302b9f2da8dff4905569fab0cff747d2
COCOLMAN <iam@hyunsol.com>
2023-02-18 03:02:03 +0900

    꼬리재귀

--- golang/FibonacciNumber/solution.go
+++ golang/FibonacciNumber/solution.go
@@ -1,19 +1,15 @@
 package FibonacciNumber
 
+func fibTail(n, b, bb int) int {
+	if n == 2 {
+		return b + bb
+	}
+	return fibTail(n-1, b+bb, b)
+}
+
 func fib(n int) int {
 	if n < 2 {
 		return n
 	}
-	answer := 0
-	current := 1
-	before := 1
-	bbefore := 0
-	for n != current {
-		answer = before + bbefore
-		bbefore = before
-		before = answer
-		current++
-	}
-	return answer
-
+	return fibTail(n, 1, 0)
 }
