2790b71c05bc4493021cca73915b5ccc5a44c7e8
COCOLMAN <iam@hyunsol.com>
2023-05-21 19:57:25 +0900

    more perform

--- golang/DotProductOfTwoSparseVectors/solution.go
+++ golang/DotProductOfTwoSparseVectors/solution.go
@@ -1,27 +1,39 @@
 package DotProductOfTwoSparseVectors
 
 type SparseVector struct {
-	vector []int
+	NonZero map[int]int
+	Length  int
 }
 
 func Constructor(nums []int) SparseVector {
-	return SparseVector{
-		vector: nums,
+	svec := SparseVector{
+		NonZero: map[int]int{},
+		Length:  len(nums),
 	}
+
+	for idx, num := range nums {
+		if num != 0 {
+			svec.NonZero[idx] = num
+		}
+	}
+
+	return svec
 }
 
 // Return the dotProduct of two sparse vectors
 func (this *SparseVector) dotProduct(vec SparseVector) int {
-	var result int
-	for i, num := range this.vector {
-		result += num * vec.vector[i]
+	i := 0
+	result := 0
+	for i < this.Length {
+
+		val1, exist1 := this.NonZero[i]
+		val2, exist2 := vec.NonZero[i]
+		if !exist2 || !exist1 {
+			goto next
+		}
+		result += val1 * val2
+	next:
+		i++
 	}
 	return result
 }
-
-/**
- * Your SparseVector object will be instantiated and called as such:
- * v1 := Constructor(nums1);
- * v2 := Constructor(nums2);
- * ans := v1.dotProduct(v2);
- */
