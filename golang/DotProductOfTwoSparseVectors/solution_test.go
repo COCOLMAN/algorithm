ba8cf78ba13d185332ef8dccccb580e1abd9bbba
COCOLMAN <iam@hyunsol.com>
2023-05-21 19:53:34 +0900

    fix wrong test case

--- golang/DotProductOfTwoSparseVectors/solution_test.go
+++ golang/DotProductOfTwoSparseVectors/solution_test.go
@@ -22,7 +22,7 @@ func TestConstructor(t *testing.T) {
 	v1 = Constructor([]int{0, 1, 0, 0, 2, 0, 0})
 	v2 = Constructor([]int{1, 0, 0, 0, 3, 0, 4})
 
-	if v1.dotProduct(v2) != 0 {
+	if v1.dotProduct(v2) != 6 {
 		t.Errorf("Fail")
 	}
 }
