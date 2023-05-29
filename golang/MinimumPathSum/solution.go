9be9e33ad767e17ff2e65f58165ab911b0aba088
COCOLMAN <iam@hyunsol.com>
2023-05-29 19:30:48 +0900

    done

---
+++ golang/MinimumPathSum/solution.go
@@ -0,0 +1,27 @@
+package MinimumPathSum
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func minPathSum(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+	for i := 1; i < n; i++ {
+		grid[0][i] += grid[0][i-1]
+	}
+	for i := 1; i < m; i++ {
+		grid[i][0] += grid[i-1][0]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			minNum := min(grid[i-1][j], grid[i][j-1])
+			grid[i][j] += minNum
+		}
+	}
+	return grid[m-1][n-1]
+}
