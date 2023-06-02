414b9aedb5ce0c5bc2a877d192f066af76f76754
soul.h <soul.h@kakaocorp.com>
2023-06-02 23:26:06 +0900

    init

---
+++ golang/MaximalSquare/solution_test.go
@@ -0,0 +1,53 @@
+package MaximalSquare
+
+import "testing"
+
+func Test_maximalSquare(t *testing.T) {
+	type args struct {
+		matrix [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "default",
+			args: args{
+				matrix: [][]byte{
+					{'1', '0', '1', '0', '0'},
+					{'1', '0', '1', '1', '1'},
+					{'1', '1', '1', '1', '1'},
+					{'1', '0', '0', '1', '0'},
+				},
+			},
+			want: 4,
+		},
+		{
+			name: "default",
+			args: args{
+				matrix: [][]byte{
+					{'0', '1'},
+					{'1', '0'},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "default",
+			args: args{
+				matrix: [][]byte{
+					{'0'},
+				},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximalSquare(tt.args.matrix); got != tt.want {
+				t.Errorf("maximalSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
