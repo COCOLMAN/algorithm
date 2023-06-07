12a74c99da7c73e1183f45d393e5b4fe3833eda4
COCOLMAN <iam@hyunsol.com>
2023-06-07 23:59:15 +0900

    done

--- golang/FindAnagramMapping/solution.go
+++ golang/FindAnagramMapping/solution.go
@@ -1,5 +1,13 @@
 package FindAnagramMapping
 
 func anagramMappings(nums1 []int, nums2 []int) []int {
-	return []int{}
+	indexMap := map[int]int{}
+	for idx, n2 := range nums2 {
+		indexMap[n2] = idx
+	}
+	var result []int
+	for _, n1 := range nums1 {
+		result = append(result, indexMap[n1])
+	}
+	return result
 }
