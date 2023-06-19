5c8f375a3038a2741928cf841414026c907d5f0d
soul.h <soul.h@kakaocorp.com>
2023-06-19 21:46:10 +0900

    done

--- golang/EditDistance/solution.go
+++ golang/EditDistance/solution.go
@@ -1,5 +1,9 @@
 package EditDistance
 
+import "fmt"
+
+var cache map[string]int
+
 func min(candidate ...int) int {
 	var m int
 	m = candidate[0]
@@ -11,8 +15,15 @@ func min(candidate ...int) int {
 	return m
 }
 
-func cal(word1, word2 string, op int) int {
+func getKey(k1, k2 string) string {
+	return fmt.Sprintf("%s:%s", k1, k2)
+}
 
+func cal(word1, word2 string, op int) int {
+	key := getKey(word1, word2)
+	if val, exist := cache[key]; exist {
+		return op + val
+	}
 	if len(word2) == 0 {
 		return op + len(word1)
 	}
@@ -21,20 +32,25 @@ func cal(word1, word2 string, op int) int {
 	}
 
 	if word1[0] == word2[0] {
-		return min(
+		val := min(
 			cal(word1[1:], word2[1:], op),   // match
 			cal(word1[1:], word2[1:], op+1), // replace
 			cal(word1[1:], word2, op+1),     // delete
 			cal(word1, word2[1:], op+1),     // insert
 		)
+		cache[key] = val - op
+		return val
 	}
-	return min(
+	val := min(
 		cal(word1[1:], word2[1:], op+1),
 		cal(word1[1:], word2, op+1),
 		cal(word1, word2[1:], op+1),
 	)
+	cache[key] = val - op
+	return val
 }
 
 func minDistance(word1 string, word2 string) int {
+	cache = map[string]int{}
 	return cal(word1, word2, 0)
 }
