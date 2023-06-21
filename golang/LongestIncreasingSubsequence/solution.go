5dc49b53112299092926f9e9009933ab96ab9c9b
soul.h <soul.h@kakaocorp.com>
2023-06-21 22:51:32 +0900

    done

--- golang/LongestIncreasingSubsequence/solution.go
+++ golang/LongestIncreasingSubsequence/solution.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func cal(currentNum, cursor, currentSize int, nums []int) int {
 	fmt.Println(currentNum, cursor, currentSize, nums)
-	if cursor == len(nums) {
+	if cursor >= len(nums) {
 		return currentSize
 	}
 
@@ -27,8 +27,21 @@ func lengthOfLIS(nums []int) int {
 	if len(nums) == 1 {
 		return 1
 	}
-	return max(
-		cal(nums[0], 1, 1, nums),
-		cal(nums[1], 2, 1, nums),
-	)
+	maxResult := 0
+
+	for idx, _ := range nums {
+		var a int
+		if idx < len(nums)-1 {
+			a = max(
+				cal(nums[idx], idx+1, 1, nums),
+				cal(nums[idx+1], idx+2, 1, nums),
+			)
+		} else {
+			a = cal(nums[idx], idx+1, 1, nums)
+		}
+		if maxResult < a {
+			maxResult = a
+		}
+	}
+	return maxResult
 }
