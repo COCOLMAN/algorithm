3cbcc8f45437fd70e15c52cbb85219387317b278
COCOLMAN <iam@hyunsol.com>
2023-06-25 18:43:43 +0900

    done

--- golang/LongestIncreasingSubsequence/solution.go
+++ golang/LongestIncreasingSubsequence/solution.go
@@ -1,62 +1,39 @@
 package LongestIncreasingSubsequence
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func cal(currentNum int, currentSize int, nums, accum []int) int {
-	//fmt.Println(currentNum, currentSize, nums, accum)
-	if len(nums) == 0 {
-		return currentSize
-	}
-	first := true
-	for idx, num := range nums {
-		if num > currentNum {
-			if !first && (num == accum[len(accum)-1]) {
-				break
-			}
-
-			first = false
-
-			return max(
-				cal(num, currentSize+1, nums[idx+1:], accum[idx+1:]),
-				cal(currentNum, currentSize, nums[idx+1:], accum[idx+1:]),
-			)
+func BSearchIdx(target int, nums []int) int {
+	start := 0
+	end := len(nums) - 2
+	for start <= end {
+		mid := (start + end) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target && target <= nums[mid+1] {
+			return mid + 1
+		}
+		if target < nums[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
 		}
 	}
-	return currentSize
+
+	return ((start + end) / 2) + 1
 }
 
 func lengthOfLIS(nums []int) int {
-	//fmt.Println(nums)
-	accumNums := []int{nums[0]}
-	for idx, num := range nums {
-		if idx == 0 {
-			continue
-		}
-		if num > accumNums[len(accumNums)-1] {
-			accumNums = append(accumNums, num)
-		} else {
-			accumNums = append(accumNums, accumNums[len(accumNums)-1])
-		}
-	}
-	//fmt.Println(accumNums)
-	maxSize := cal(nums[0], 1, nums[1:], accumNums[1:])
-	smallerNum := nums[0]
+	increasingNums := []int{nums[0]}
 
-	for idx, num := range nums {
-		if smallerNum <= num {
-			continue
+	for _, num := range nums[1:] {
+		if increasingNums[len(increasingNums)-1] < num {
+			increasingNums = append(increasingNums, num)
+		} else if increasingNums[0] >= num {
+			increasingNums[0] = num
 		} else {
-			smallerNum = num
-		}
-		a := cal(num, 1, nums[idx+1:], accumNums[idx+1:])
-		if a > maxSize {
-			maxSize = a
+			idx := BSearchIdx(num, increasingNums)
+			increasingNums[idx] = num
 		}
 	}
-	return maxSize
+
+	return len(increasingNums)
 }
